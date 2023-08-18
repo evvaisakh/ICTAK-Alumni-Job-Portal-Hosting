82796d2340ac70d57eb9fc0c977a7aa10238019c
Vaisakh Vijayan <evvaisakh4@gmail.com>
2023-08-18 23:36:36 +0530

    first commit

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ const path = require('path');
 const PORT = 3000;
 const db = require("./middlewares/db");
 
-
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use('/',api);
+app.use('/api',api);
 
 app.get('/download/:resume',(req,res)=>{
     console.log(res);
     res.sendFile(path.join(__dirname,`./uploads/${req.params.resume}`));
 });
 
-app.use(express.static('./dist/frontend'));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 
 // last route
 app.get('*', async(req, res)=>{
-    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(PORT, ()=>{
